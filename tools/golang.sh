8410e28ed5b261cb5d154eff1fc736b51759214c
Daniel Menchaca Luna <daniel@localhost>
2021-04-02 23:37:14 -0600

    Add golang installer

--- tools/golang.sh
+++ tools/golang.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#Get the laster golang release
+#
+# Get the laster golang release
 GO_PACKAGE_INSTALLER="$(curl https://golang.org/VERSION?m=text)".linux-amd64.tar.gz
 FILE_GO_PACKAGE=/tmp/$GO_PACKAGE_INSTALLER
 [ -e $FILE_GO_PACKAGE ] && rm $FILE_GO_PACKAGE
-wget "https://dl.google.com/go/$GO_PACKAGE_INSTALLER" -O $FILE_GO_PACKAGE 
+#
+#
+# Get The golang package
+wget "https://dl.google.com/go/$GO_PACKAGE_INSTALLER" -O $FILE_GO_PACKAGE
+#
 # "Install" golang in the system
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $FILE_GO_PACKAGE 
