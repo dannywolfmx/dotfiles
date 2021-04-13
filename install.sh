26b8f9177f2cd0c0ec4594139945b6c80386d8bc
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2021-04-13 09:10:29 -0500

    Add questions to the instalal.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-#Run install emacs
-. $PWD/emacs/install.sh 
-. $PWD/tools/golang.sh
+
+#Ask to install emacs
+echo -n "Install emacs? [Y/n]"
+read answer 
+if [ "$answer" != "${answer#[Yy]}"]
+	#Run install emacs
+	. $PWD/emacs/install.sh 
+fi
+
+echo -n "Install Go? [Y/n]"
+read answer 
+if [ "$answer" != "${answer#[Yy]}"]
+	#Run install golang
+	. $PWD/tools/golang.sh
+fi
+
 #.bashrc
 rm $HOME/.bashrc
 ln -s $PWD/.bashrc $HOME/.bashrc
