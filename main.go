e21cdb8e5d434f9423c82c2d9aec6534bfbc74f9
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2022-07-22 17:51:42 -0500

    Add example of multiselect

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-import "gopkg.in/AlecAivazis/survey.v1"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+
+	"gopkg.in/AlecAivazis/survey.v1"
+)
 
 func main() {
-	text := ""
-	prompt := &survey.Multiline{
-		Message: "ping",
+	programs := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "Que deseas instalar?",
+		Options: []string{"vim", "emacs", "go", "zsh"},
 	}
-	survey.AskOne(prompt, &text, nil)
+	survey.AskOne(prompt, &programs, nil)
+
+	autoinstall(programs)
+}
+
+func autoinstall(programs []string) {
+	distro := "Ubuntu"
+	install(distro, programs)
+}
+
+func install(distro string, programs []string) {
+	var cmd *exec.Cmd
+
+	args := []string{"apt-get", "install", "-y"}
+	args = append(args, programs...)
+
+	fmt.Print(args)
+	if "Ubuntu" == distro {
+		cmd = exec.Command("sudo", args...)
+	} else if "Fedora" == distro {
+		cmd = exec.Command("dnf", programs...)
+	}
+
+	var outbuf, errbuf bytes.Buffer
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+
+	err := cmd.Run()
+	stdout := outbuf.String()
+
+	log.Printf("command result, stdout: %v, stderr: %v, exitCode: %v", stdout, errbuf.String(), err)
+	return
 }
