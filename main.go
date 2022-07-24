68e12316ad0bf5083a33b48bcecc2dfa6a6b8691
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2022-07-24 10:45:30 -0500

    Add custom file to the prompt functions

--- main.go
+++ main.go
@@ -1,50 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"log"
-	"os/exec"
 
-	"gopkg.in/AlecAivazis/survey.v1"
+	"github.com/dannywolfmx/dotfiles/prompt"
 )
 
 func main() {
-	programs := []string{}
-	prompt := &survey.MultiSelect{
-		Message: "Que deseas instalar?",
-		Options: []string{"vim", "emacs", "go", "zsh"},
-	}
-	survey.AskOne(prompt, &programs, nil)
-
-	autoinstall(programs)
-}
-
-func autoinstall(programs []string) {
-	distro := "Ubuntu"
-	install(distro, programs)
-}
-
-func install(distro string, programs []string) {
-	var cmd *exec.Cmd
-
-	args := []string{"apt-get", "install", "-y"}
-	args = append(args, programs...)
-
-	fmt.Print(args)
-	if "Ubuntu" == distro {
-		cmd = exec.Command("sudo", args...)
-	} else if "Fedora" == distro {
-		cmd = exec.Command("dnf", programs...)
+	recipe, err := prompt.ReadRecipeFile("./recipe.yml")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	var outbuf, errbuf bytes.Buffer
-	cmd.Stdout = &outbuf
-	cmd.Stderr = &errbuf
-
-	err := cmd.Run()
-	stdout := outbuf.String()
-
-	log.Printf("command result, stdout: %v, stderr: %v, exitCode: %v", stdout, errbuf.String(), err)
-	return
+	prompt.NewPrompt(recipe).Show()
 }
